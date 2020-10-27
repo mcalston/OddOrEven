9471de65d28871e9b2005e77b3e895e3a365084d
Miles Alston <milescalston@gmail.com>
2020-10-27 13:01:13 -0400

    ready to go

--- OddOrEven/Program.cs
+++ OddOrEven/Program.cs
@@ -6,7 +6,31 @@ namespace OddOrEven
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool answer = IsOdd(6);
+           Console.WriteLine(IsOdd(6));
+        }
+
+        public static bool IsOdd(int num)
+        {
+            Console.WriteLine("Enter number");
+            num = int.Parse(Console.ReadLine());
+
+
+            if (num % 2 == 0) 
+            {
+
+                Console.WriteLine("Your number is odd");
+
+                return true;
+            }
+            else
+
+            {
+
+                Console.WriteLine("Your number is even");
+                return false;
+            }
+
         }
     }
 }
